436e18f57dcf4e79a86975b10b8b10140941a393
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-10-21 22:13:16 +0300

    writing UI test

---
+++ Game Of ThronesTests/Game_Of_ThronesTests.swift
@@ -0,0 +1,18 @@
+//
+//  Game_Of_ThronesTests.swift
+//  Game Of ThronesTests
+//
+//  Created by Amby on 21/10/2022.
+//
+
+import XCTest
+
+class Game_Of_ThronesTests: XCTestCase {
+    let app = XCUIApplication()
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        app.launch()
+    }
+
+}
