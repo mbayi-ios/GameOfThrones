d8839dc428b668b3df5a0403d71eb8bc0e960fed
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-10-21 13:55:40 +0300

    Initial Commit

---
+++ GameOfThrones/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameOfThrones
+//
+//  Created by Amby on 21/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GameOfThrones/GameOfThronesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfThronesApp.swift
+//  GameOfThrones
+//
+//  Created by Amby on 21/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct GameOfThronesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
